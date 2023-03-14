42f905a21ae08d844fa6adf307e388a108caaad9
SeungWookHan <hswook12@me.com>
2023-03-14 17:37:22 +0900

    Ts-fy did controller

--- backend/src/routes/did.ts
+++ backend/src/routes/did.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import DIDService from "../services/didService.js";
+import DIDService from "../services/didService";
 
 const didService = new DIDService();
 
@@ -8,6 +8,19 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   try {
     const result = await didService.getAlldids();
+    return res.status(200).send(result);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await didService.getDid(id);
+    if (!result) {
+      return res.status(404).send({ message: "Did not found" });
+    }
     return res.status(200).send(result);
   } catch (err) {
     console.log(err);
@@ -34,6 +47,15 @@ router.put("/:id/status", async (req, res) => {
   try {
     const { id } = req.params;
     const { status } = req.body;
+    const did = await didService.getDid(id);
+    if (!did) {
+      return res.status(404).send({ message: "Did not found" });
+    }
+    if (did.status === "published") {
+      return res.status(400).send({ message: "Did is already published" });
+    }
+    const hash = "12341234";
+    await didService.sendDidToBlockchain(hash, did.studentId);
     const result = await didService.updateDidStatus(id, status);
     return res.status(201).send(result);
   } catch (err) {
