bc808e2e8ce1e268a2fdd57c75b31fcf3317f162
SeungWookHan <hswook12@me.com>
2023-03-16 15:55:07 +0900

    Add getCertificate converter

--- backend/src/contract/did.ts
+++ backend/src/contract/did.ts
@@ -31,8 +31,10 @@ export default class DID implements Contract {
     });
   }
 
-  // async getCertificate(provider: ContractProvider, student_id: number) {
-  //   const [stud_id, hash] = await provider.get("certificate", [student_id]);
-  //   return [stud_id, hash] as const;
-  // }
+  async getCertificate(provider: ContractProvider, studentId: number) {
+    const result = await provider.get("certificate", [
+      { type: "int", value: BigInt(studentId) },
+    ]);
+    return result.stack;
+  }
 }
